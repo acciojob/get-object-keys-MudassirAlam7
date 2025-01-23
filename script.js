8345aad482b8420280c62f8a9a2b6e9134343246
MudassirAlam7 <mudassiralam7@gmail.com>
2025-01-23 08:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let student = {
+	name : "Mudassir alam"
+}
+function getKeys(obj) {
+	return Object.keys(obj)
+}
+let keys = getKeys(student)
+console.log(keys)
+
+if(keys.includes('name')){
+	console.log('yes the name key exit in the object')
+}
+else{
+	console.log('No the name does not exit in the object')
+}
+
